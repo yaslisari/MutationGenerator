0111bf13f6323522e297f65af3804127af913760
yaslisari <yaslisari@gmail.com>
2013-04-11 21:29:08 -0400

    hydrophilicity and non-polarity added. input style changed. NOT fully tested.

--- src/ChainAAcid.java
+++ src/ChainAAcid.java
@@ -1,15 +1,17 @@
 
 public class ChainAAcid extends AAcid {
 	int position;
-	double tendency, tendency1, tendency2;
+	double tendency, tendency1, tendency2 ,tendency3, tendency4 ;
 	
 	public ChainAAcid(char aacidn, double hphobicity, double htendency,
-			int position, double tendency, double tendency1, double tendency2) {
+			int position, double tendency, double tendency1, double tendency2, double tendency3, double tendency4) {
 		super(aacidn, hphobicity, htendency);
 		this.position = position;
 		this.tendency = tendency;
 		this.tendency1 = tendency1;
 		this.tendency2 = tendency2;
+		this.tendency3 = tendency3;
+		this.tendency4 = tendency4;
 	}
 	
 	public int chainPosition(){
@@ -26,6 +28,14 @@ public class ChainAAcid extends AAcid {
 	
 	public double polarTendency(){
 		return tendency2;
+	}
+	
+	public double hydrophilicTendency(){
+		return tendency3;
+	}
+	
+	public double nonPolarTendency(){
+		return tendency4;
 	}
 	
 	public String toString(){
